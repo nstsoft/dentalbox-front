45065ef59857029878374863ce467c722f11f229
YuriiBoikoDev <yurij.boyko@nstsoft.com>
2024-10-14 20:57:36 +0300

    Change patient page layout

--- src/pages/PatientCard/index.tsx
+++ src/pages/PatientCard/index.tsx
@@ -4,6 +4,7 @@ import { DentalMap, PatientInfo } from "./components";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { useTranslation } from "react-i18next";
+import { PatientNotes } from "./components/patientNotes";
 
 export const PatientCardPage = () => {
   const { patientId } = useParams();
@@ -14,7 +15,16 @@ export const PatientCardPage = () => {
   return (
     <Box>
       <Typography variant="h4">{t("patientCard")}</Typography>
-      <PatientInfo patient={data} />
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          mb: 1,
+        }}
+      >
+        <PatientInfo patient={data} />
+        <PatientNotes patient={data} />
+      </Box>
       <DentalMap patientId={patientId} />
     </Box>
   );
